b184090730c202920bdc65bac8c9a698a032f487
Ryan Roemer <ryan.roemer@formidablelabs.com>
2013-10-26 16:21:39 -0700

    More amd/non-amd infrastructure.

--- test/js/spec/chai-jq.spec.js
+++ test/js/spec/chai-jq.spec.js
@@ -1,7 +1,18 @@
-describe("chai-jq", function () {
-  describe("setup", function () {
-    it("patches native chai", function () {
-      expect(chai).to.be.ok;
+// Make AMD/Non-AMD compatible (boilerplate).
+if (typeof define !== "function") {
+  /*global define:true */
+  var define = function (deps, callback) {
+    // Use global jQuery and Chai for non-AMD shim.
+    callback(window.$, window.chai);
+  };
+}
+
+define(["jquery", "chai"], function ($, chai) {
+  describe("chai-jq", function () {
+    describe("setup", function () {
+      it("patches native chai", function () {
+        expect(chai).to.be.ok;
+      });
     });
   });
 });
